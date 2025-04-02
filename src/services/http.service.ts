6e8e70bb09e15fa10f9b09befb812a3b2ef5a131
Daniil <krupa.daniil94@outlook.com>
2025-04-02 20:54:59 +0300

    Додано компонент та методы в http service

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,89 +1,192 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
-import {IUserReg} from '../interfaces/user-auth/i-user-reg';
-import {catchError, map, Observable, of} from 'rxjs';
-import {IUserLogin} from '../interfaces/user-auth/i-user-login';
-import {IError} from '../interfaces/i-error';
-import {AuthService} from './auth.service';
-import {JWTResponse} from '../interfaces/user-auth/jwtresponse';
-import {ITokenData} from '../interfaces/user-auth/i-token-data';
-import {IUserStartData} from '../interfaces/user-auth/i-user-start-data';
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpResponse,
+} from '@angular/common/http';
+import { IUserReg } from '../interfaces/user-auth/i-user-reg';
+import { catchError, map, Observable, of } from 'rxjs';
+import { IUserLogin } from '../interfaces/user-auth/i-user-login';
+import { IError } from '../interfaces/i-error';
+import { AuthService } from './auth.service';
+import { JWTResponse } from '../interfaces/user-auth/jwtresponse';
+import { ITokenData } from '../interfaces/user-auth/i-token-data';
+import { IUserStartData } from '../interfaces/user-auth/i-user-start-data';
+import { IUser } from '../interfaces/i-user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
   private readonly baseUrl: string;
 
   constructor(private http: HttpClient, private authService: AuthService) {
-    this.baseUrl = "http://localhost:8080";
+    this.baseUrl = 'http://localhost:8080';
   }
 
-  registrationUser(user:IUserReg):Observable<boolean | IError> {
-    return this.http.post(`${this.baseUrl}/registration`, user, {observe: "response"}).pipe(
-      map(response=>{
-        return response.status === 200;
-      } ),
-      catchError((error:HttpErrorResponse)=> {
-        const errorBody = error.error?.body;
-        const errorObj: IError = {
-          status: errorBody?.status || error.status,
-          message: errorBody?.message || "User with that name or email already exists",
-          timestamp: errorBody.timestamp || new Date()
-        }
-
-        return of(errorObj);
-      })
-    );
-  }
-
-  loginUser(user:IUserLogin):Observable<IUserStartData | IError | null> {
-    return this.http.post<any>(this.baseUrl + "/login", user, {observe: "response"}).pipe(
-
-      map((response: HttpResponse<any>): IUserStartData | IError | null => {
-
-        if (response.status === 200) {
-          const jwtResponse: JWTResponse =  {
-            token: response.body.body.token
+  registrationUser(user: IUserReg): Observable<boolean | IError> {
+    return this.http
+      .post(`${this.baseUrl}/registration`, user, { observe: 'response' })
+      .pipe(
+        map((response) => {
+          return response.status === 200;
+        }),
+        catchError((error: HttpErrorResponse) => {
+          const errorBody = error.error?.body;
+          const errorObj: IError = {
+            status: errorBody?.status || error.status,
+            message:
+              errorBody?.message ||
+              'User with that name or email already exists',
+            timestamp: errorBody.timestamp || new Date(),
           };
-          this.authService.setToken(jwtResponse.token);
-          const data: ITokenData|null = this.authService.getDecodeToken();
-          if (data != null) {
-            return {
-              roles: data.roles,
-              username: data.sub
+
+          return of(errorObj);
+        })
+      );
+  }
+
+  loginUser(user: IUserLogin): Observable<IUserStartData | IError | null> {
+    return this.http
+      .post<any>(this.baseUrl + '/login', user, { observe: 'response' })
+      .pipe(
+        map((response: HttpResponse<any>): IUserStartData | IError | null => {
+          if (response.status === 200) {
+            const jwtResponse: JWTResponse = {
+              token: response.body.body.token,
             };
+            this.authService.setToken(jwtResponse.token);
+            const data: ITokenData | null = this.authService.getDecodeToken();
+            if (data != null) {
+              return {
+                roles: data.roles,
+                username: data.sub,
+              };
+            }
+            return null;
+          } else {
+            return response.body.body as IError;
           }
-          return null;
-        } else {
-          return response.body.body as IError;
-        }
-      }),
-      catchError((error: HttpErrorResponse) => {
-        const errorBody = error.error?.body;
-        const errorObj: IError = {
-          status: errorBody?.status || error.status,
-          message: errorBody?.message || "Authorization error",
-          timestamp: errorBody?.timestamp || new Date()
-        }
+        }),
+        catchError((error: HttpErrorResponse) => {
+          const errorBody = error.error?.body;
+          const errorObj: IError = {
+            status: errorBody?.status || error.status,
+            message: errorBody?.message || 'Authorization error',
+            timestamp: errorBody?.timestamp || new Date(),
+          };
 
-        return of(errorObj);
+          return of(errorObj);
+        })
+      );
+  }
+
+  loadingAllUsers(): Observable<IUser[] | IError> {
+    return this.http
+      .get<Object>(`${this.baseUrl}/all-users`, { observe: 'response' })
+      .pipe(
+        map((response: HttpResponse<Object>): IUser[] | IError => {
+          if (response.status === 200) {
+            const users: IUser[] = response.body as IUser[];
+
+            return users;
+          } else {
+            const error = response.body as IError;
+            return error;
+          }
+        })
+      );
+  }
+
+  loadingUserById(id: number): Observable<IUser | IError> {
+    return this.http
+      .get<Object>(`${this.baseUrl}/user/${id}`, { observe: 'response' })
+      .pipe(
+        map((response: HttpResponse<Object>): IUser | IError => {
+          if (response.status === 200) {
+            const user: IUser = response.body as IUser;
+
+            return user;
+          } else {
+            const error = response.body as IError;
+
+            return error;
+          }
+        })
+      );
+  }
+
+  loadingUserByEmail(email: string): Observable<IUser | IError> {
+    return this.http
+      .get<Object>(`${this.baseUrl}/user/${email}`, { observe: 'response' })
+      .pipe(
+        map((response: HttpResponse<Object>): IUser | IError => {
+          if (response.status === 200) {
+            const user: IUser = response.body as IUser;
+
+            return user;
+          } else {
+            const error = response.body as IError;
+
+            return error;
+          }
+        })
+      );
+  }
+
+  blockUserById(id: number): Observable<boolean | IError> {
+    return this.http
+      .get<Object>(`${this.baseUrl}/block-user/${id}`, { observe: 'response' })
+      .pipe(
+        map((response: HttpResponse<Object>): boolean | IError => {
+          if (response.status === 200) {
+            return true;
+          } else {
+            const error = response.body as IError;
+
+            return error;
+          }
+        })
+      );
+  }
+
+  blockUserByEmail(email: string): Observable<boolean | IError> {
+    return this.http
+      .get<Object>(`${this.baseUrl}/block-user/${email}`, {
+        observe: 'response',
       })
-    );
+      .pipe(
+        map((response: HttpResponse<Object>): boolean | IError => {
+          if (response.status === 200) {
+            return true;
+          } else {
+            const error = response.body as IError;
+
+            return error;
+          }
+        })
+      );
   }
 
-  checkUsername(username:string):Observable<boolean> {
-    return this.http.get(`${this.baseUrl}/check-username/${username}`, {observe: "response"}).pipe(
-      map((response: HttpResponse<object>):boolean => {
-        return response.status === 200;
+  checkUsername(username: string): Observable<boolean> {
+    return this.http
+      .get(`${this.baseUrl}/check-username/${username}`, {
+        observe: 'response',
       })
-    );
+      .pipe(
+        map((response: HttpResponse<object>): boolean => {
+          return response.status === 200;
+        })
+      );
   }
 
-  checkEmail(email:string):Observable<boolean> {
-    return this.http.get(`${this.baseUrl}/check-email/${email}`, {observe: "response"}).pipe(
-      map((response: HttpResponse<object>):boolean => response.status === 200)
-    );
+  checkEmail(email: string): Observable<boolean> {
+    return this.http
+      .get(`${this.baseUrl}/check-email/${email}`, { observe: 'response' })
+      .pipe(
+        map(
+          (response: HttpResponse<object>): boolean => response.status === 200
+        )
+      );
   }
-
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,108 +1,129 @@
-import {inject, Injectable} from '@angular/core';
-import {EntityStorage} from '../storage/entity.storage';
-import {IUserStartData} from '../interfaces/user-auth/i-user-start-data';
-import {IUserLogin} from '../interfaces/user-auth/i-user-login';
-import {HttpService} from './http.service';
-import {IError} from '../interfaces/i-error';
-import {MessageService} from './message.service';
-import {IUserReg} from '../interfaces/user-auth/i-user-reg';
-import {ActivatedRoute, Params, Router} from '@angular/router';
-import {AuthService} from './auth.service';
-import {catchError, map, Observable, of, switchMap} from 'rxjs';
-import {ITokenData} from '../interfaces/user-auth/i-token-data';
-import {UserStartData} from '../models/user-start-data';
+import { inject, Injectable } from '@angular/core';
+import { EntityStorage } from '../storage/entity.storage';
+import { IUserStartData } from '../interfaces/user-auth/i-user-start-data';
+import { IUserLogin } from '../interfaces/user-auth/i-user-login';
+import { HttpService } from './http.service';
+import { IError } from '../interfaces/i-error';
+import { MessageService } from './message.service';
+import { IUserReg } from '../interfaces/user-auth/i-user-reg';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { catchError, map, Observable, of, switchMap } from 'rxjs';
+import { ITokenData } from '../interfaces/user-auth/i-token-data';
+import { UserStartData } from '../models/user-start-data';
+import { IUser } from '../interfaces/i-user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   readonly store = inject(EntityStorage);
 
-  constructor(private http: HttpService,
-              private messageService: MessageService,
-              private activeRoute:ActivatedRoute,
-              private router:Router,
-              private authService: AuthService) {}
+  constructor(
+    private http: HttpService,
+    private messageService: MessageService,
+    private activeRoute: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
-  singIn(user: IUserLogin):Observable<boolean>{
+  singIn(user: IUserLogin): Observable<boolean> {
     return this.http.loginUser(user).pipe(
-      switchMap((item:IUserStartData|IError|null): Observable<boolean> => {
-      if (item !== null) {
-        if (this.isUserStartData(item)) {
-          this.store.addUserStartData(item as IUserStartData);
-          this.router.navigate(["/"]).then();
-          return of(true);
-        } else  if (this.isError(item)) {
-          return this.http.checkUsername(user.username).pipe(
-            map((res: boolean):boolean => {
-            res ? this.messageService.setMessage("Incorrect password entered") :
-              this.messageService.setMessage("Such a user does not exist");
-              return false;
-            })
-          )
+      switchMap((item: IUserStartData | IError | null): Observable<boolean> => {
+        if (item !== null) {
+          if (this.isUser(item)) {
+            this.store.addUserStartData(item as IUserStartData);
+            this.router.navigate(['/']).then();
+            return of(true);
+          } else if (this.isError(item)) {
+            return this.http.checkUsername(user.username).pipe(
+              map((res: boolean): boolean => {
+                res
+                  ? this.messageService.setMessage('Incorrect password entered')
+                  : this.messageService.setMessage(
+                      'Such a user does not exist'
+                    );
+                return false;
+              })
+            );
+          } else {
+            this.messageService.setMessage(null);
+            return of(false);
+          }
         }
-        else {
-          this.messageService.setMessage(null);
-          return of(false);
-        }
-      }
-      return of(false);
-    }));
+        return of(false);
+      })
+    );
   }
 
-  singInOAuth2(): boolean{
+  singInOAuth2(): boolean {
     let flag: boolean = false;
-    this.activeRoute.fragment.subscribe((fragment:string | null):void => {
+    this.activeRoute.fragment.subscribe((fragment: string | null): void => {
       if (fragment !== null && fragment !== undefined) {
         const pr = new URLSearchParams(fragment);
-        const token = pr.get("token");
+        const token = pr.get('token');
         if (token) {
           this.authService.setToken(token);
-          const data: ITokenData|null = this.authService.getDecodeToken();
+          const data: ITokenData | null = this.authService.getDecodeToken();
           if (data != null) {
-            this.store.addUserStartData(new UserStartData(data.roles, data.sub))
+            this.store.addUserStartData(
+              new UserStartData(data.roles, data.sub)
+            );
             flag = true;
-            this.router.navigate(["/"]).then();
+            this.router.navigate(['/']).then();
           }
         }
       }
     });
 
-    this.activeRoute.queryParams.subscribe((params: Params):void => {
-      if (params["error"]) {
-        this.messageService.setMessage("Authorization error");
+    this.activeRoute.queryParams.subscribe((params: Params): void => {
+      if (params['error']) {
+        this.messageService.setMessage('Authorization error');
       }
-    })
+    });
 
     return flag;
   }
 
-  singUp(user: IUserReg):void {
-    this.http.registrationUser(user).subscribe({next:(item: boolean|IError):void => {
+  singUp(user: IUserReg): void {
+    this.http.registrationUser(user).subscribe({
+      next: (item: boolean | IError): void => {
         if (this.isError(item)) {
-          this.messageService.setMessage((item as unknown as IError).message)
+          this.messageService.setMessage((item as unknown as IError).message);
         } else {
           if (item === true) {
-            this.router.navigate(["/successful-registration"]).then();
+            this.router.navigate(['/successful-registration']).then();
           } else {
-            this.router.navigate(["/error-data"]).then();
+            this.router.navigate(['/error-data']).then();
           }
         }
-      }})
+      },
+    });
   }
 
-  singOut():void {
+  singOut(): void {
     this.authService.removeToken();
     this.store.removeUserStartData();
-    this.router.navigate(["/"]).then();
+    this.router.navigate(['/']).then();
   }
 
-  private isUserStartData(item: any): boolean {
-    return "roles" in item && Array.isArray(item.roles);
+  loadingAllUsers(): void {
+    this.http.loadingAllUsers().subscribe({
+      next: (item: IUser[] | IError): void => {
+        if (this.isError(item)) {
+          this.messageService.setMessage((item as unknown as IError).message);
+        } else {
+          this.store.setAllUsers(item as IUser[]);
+        }
+      },
+    });
+  }
+
+  private isUser(item: any): boolean {
+    return 'roles' in item && Array.isArray(item.roles);
   }
 
   private isError(item: any): boolean {
-
-    return "timestamp" in item && item.timestamp instanceof Date;
+    return 'timestamp' in item && item.timestamp instanceof Date;
   }
 }
