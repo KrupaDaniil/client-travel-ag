f40a3c7d632dc5d2df1bcb33732131d0fb389a15
Daniil <krupa.daniil94@outlook.com>
2025-05-14 01:50:16 +0300

    add from-to-management

---
+++ src/app/admin_controllers_group/from-to-management/from-to-management.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-from-to-management",
+	imports: [],
+	templateUrl: "./from-to-management.component.html",
+	styleUrl: "./from-to-management.component.css"
+})
+export class FromToManagementComponent {}
