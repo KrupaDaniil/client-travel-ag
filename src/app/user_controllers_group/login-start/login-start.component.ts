f1f763bd6bc316501cc79f79ebd2b68c1a542526
Nice <krupa.daniil94@outlook.com>
2025-05-28 01:29:45 +0300

    The initial template of the login page has been added. The menu has been improved.

---
+++ src/app/user_controllers_group/login-start/login-start.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-start',
+  imports: [],
+  templateUrl: './login-start.component.html',
+  styleUrl: './login-start.component.css'
+})
+export class LoginStartComponent {
+
+}
