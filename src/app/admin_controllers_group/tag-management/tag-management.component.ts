b979e45c1024169643e99c5908fe6fe26b850097
Nice <krupa.daniil94@outlook.com>
2025-06-05 00:43:30 +0300

    Added tag management component with basic structure and styles

---
+++ src/app/admin_controllers_group/tag-management/tag-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-management',
+  imports: [],
+  templateUrl: './tag-management.component.html',
+  styleUrl: './tag-management.component.css'
+})
+export class TagManagementComponent {
+
+}
