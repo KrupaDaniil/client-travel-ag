fc726d3ffaab0bd8d01ef24c375f75369141e67f
Daniil <krupa.daniil94@outlook.com>
2025-06-22 12:24:37 +0300

    Added a new view for the main tours and updated custom information styles

---
+++ src/app/user_controllers_group/main-tours/main-tours.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-tours',
+  imports: [],
+  templateUrl: './main-tours.component.html',
+  styleUrl: './main-tours.component.css'
+})
+export class MainToursComponent {
+
+}
