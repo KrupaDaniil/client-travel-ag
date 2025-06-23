2977ff685fd0c389d1d973c2c926b0efe78070a8
Nice <krupa.daniil94@outlook.com>
2025-06-24 00:55:04 +0300

    Updated design and functionality of components: added search forms in the main tour, improved data processing, added user role checks, changed buttons and styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,54 @@
-import {Component, computed, inject} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import {HttpService} from '../services/http.service';
-import {NgOptimizedImage} from '@angular/common';
-import {AuthService} from '../services/auth.service';
-import {EntityStorage} from '../storage/entity.storage';
-import {UserService} from '../services/user.service';
+import { Component, computed, inject } from "@angular/core";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { HttpService } from "../services/http.service";
+import { NgOptimizedImage } from "@angular/common";
+import { AuthService } from "../services/auth.service";
+import { EntityStorage } from "../storage/entity.storage";
+import { UserService } from "../services/user.service";
+import { UserRoles } from "./enums/user-roles";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, NgOptimizedImage, RouterLink],
-  providers: [HttpService, AuthService, UserService],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: "app-root",
+	imports: [RouterOutlet, NgOptimizedImage, RouterLink],
+	providers: [HttpService, AuthService, UserService],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css"
 })
 export class AppComponent {
-  private readonly store = inject(EntityStorage);
+	private readonly store = inject(EntityStorage);
 
-  username = computed(() => this.store.username());
+	username = computed(() => this.store.username());
 
-  constructor(private http: HttpService, private auth: AuthService, private userService: UserService) {
-  }
+	constructor(private http: HttpService, private auth: AuthService, private userService: UserService) {}
 
-  isLogin(): boolean {
-    return this.auth.isAuthenticated();
-  }
+	isLogin(): boolean {
+		if (this.auth.isAuthenticated() && !this.auth.isTokenExpired()) {
+			return true;
+		} else {
+			this.auth.removeToken();
+			return false;
+		}
+	}
 
-  onLogout(): void {
-    this.userService.singOut();
-  }
+	onLogout(): void {
+		this.userService.singOut();
+	}
+
+	checkUserRole(): boolean {
+		if (this.store.roles().length === 0) {
+			return false;
+		}
+
+		for (const role of this.store.roles()) {
+			if (role === UserRoles.ADMIN) {
+				return true;
+			}
+
+			if (role === UserRoles.MANAGER) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
--- src/app/user_controllers_group/main-tours/main-tours.component.ts
+++ src/app/user_controllers_group/main-tours/main-tours.component.ts
@@ -3,30 +3,135 @@ import { LoadingComponent } from "../../loading/loading.component";
 import { ICardTour } from "../../../interfaces/tour-block/i-card-tour";
 import { EntityStoragePr2 } from "../../../storage/entity.storage.pr2";
 import { TourService } from "../../../services/tour.service";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @Component({
 	selector: "app-main-tours",
-	imports: [LoadingComponent],
+	imports: [LoadingComponent, ReactiveFormsModule, CommonModule],
+	providers: [TourService],
 	templateUrl: "./main-tours.component.html",
 	styleUrl: "./main-tours.component.css"
 })
 export class MainToursComponent implements OnInit {
 	private readonly store = inject(EntityStoragePr2);
 	protected allTours: Signal<ICardTour[]> = computed((): ICardTour[] => this.store.clientToursEntities());
+	protected displayTours: WritableSignal<ICardTour[] | null> = signal<ICardTour[] | null>(null);
 	protected isLoadingData: WritableSignal<boolean> = signal<boolean>(false);
-	private timerId: number | undefined;
+	protected minPrice: number | undefined;
+	protected maxPrice: number | undefined;
+
+	protected searchForm: FormGroup | undefined;
 
 	constructor(private tourService: TourService) {
 		this.isLoadingStatus();
 	}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.createSearchForm();
+	}
 
 	private isLoadingStatus(): void {
 		effect(() => {
-			this.tourService.setAllClientTour().then(() => {
+			this.tourService.setAllClientTour().then((): void => {
+				this.displayTours.set(this.allTours());
 				this.isLoadingData.set(true);
+				this.setMinMaxValue();
 			});
 		});
+	}
+	private createSearchForm(): void {
+		this.searchForm = new FormGroup({
+			tourName: new FormControl(""),
+			minPrice: new FormControl<number | null>(null),
+			maxPrice: new FormControl<number | null>(null)
+		});
+	}
+
+	protected searchData(): void {
+		if (this.searchForm) {
+			const values = this.searchForm.value;
+			const tourName: string = values.tourName as string;
+			const minPrice: number = values.minPrice as number;
+			const maxPrice: number = values.maxPrice as number;
+
+			let tmpArr: ICardTour[] = [];
+			let globalArr: ICardTour[] = [];
+
+			if (values) {
+				if (tourName.trim() !== "") {
+					tmpArr = this.allTours().filter((t: ICardTour): boolean =>
+						t.name.toLowerCase().includes(tourName.toLowerCase())
+					);
+				}
+
+				if (tmpArr.length > 0) {
+					for (const tN of tmpArr) {
+						globalArr.push(tN);
+					}
+					tmpArr = [];
+				}
+
+				console.log(globalArr);
+
+				if (this.minPrice && this.maxPrice) {
+					if (minPrice > this.minPrice && maxPrice === this.maxPrice) {
+						tmpArr = this.allTours().filter((t: ICardTour): boolean => t.price >= minPrice);
+					}
+
+					if (minPrice === this.minPrice && maxPrice < this.maxPrice) {
+						tmpArr = this.allTours().filter((t: ICardTour): boolean => t.price <= maxPrice);
+					}
+
+					if (minPrice > this.minPrice && maxPrice < this.maxPrice) {
+						tmpArr = this.allTours().filter((t: ICardTour): boolean => t.price >= minPrice && t.price <= maxPrice);
+					}
+
+					if (minPrice === this.maxPrice) {
+						tmpArr = this.allTours().filter((t: ICardTour): boolean => t.price === maxPrice);
+					}
+
+					if (tmpArr.length > 0) {
+						for (const tP of tmpArr) {
+							globalArr.push(tP);
+						}
+						tmpArr = [];
+					}
+				}
+
+				if (globalArr.length > 0) {
+					console.log(globalArr);
+					this.displayTours.set(globalArr);
+				}
+			}
+		}
+	}
+
+	protected clearSearch(): void {
+		if (this.searchForm) {
+			this.displayTours.set(this.allTours());
+			this.searchForm.get("tourName")!.setValue("");
+			this.setMinMaxValue();
+		}
+	}
+
+	protected convertDate(date: string | Date): string {
+		const currentDate: Date = new Date(date);
+		const month: string = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+
+		return `${currentDate.getDate().toString().padStart(2, "0")}.${month}.${currentDate.getFullYear()}`;
+	}
+
+	private setMinMaxValue(): void {
+		const price: number[] = this.allTours().map((t: ICardTour): number => t.price);
+		this.minPrice = Math.min(...price);
+		this.maxPrice = Math.max(...price);
+
+		if (this.minPrice && this.maxPrice && this.minPrice >= 0 && this.maxPrice > 0) {
+			if (this.searchForm && this.searchForm.get("minPrice") && this.searchForm.get("maxPrice")) {
+				this.searchForm.get("minPrice")!.setValue(this.minPrice);
+				this.searchForm.get("maxPrice")!.setValue(this.maxPrice);
+			}
+		}
 	}
 }
